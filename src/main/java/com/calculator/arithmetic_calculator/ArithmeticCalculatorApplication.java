e7bd4517ef4301eb4a2c1811ae21baa55105c30a
Orlando Cano <orlando.cano_roman@dhl.com>
2023-09-27 18:44:50 -0600

    configuration to support calls from a ReactJS app

--- src/main/java/com/calculator/arithmetic_calculator/ArithmeticCalculatorApplication.java
+++ src/main/java/com/calculator/arithmetic_calculator/ArithmeticCalculatorApplication.java
@@ -2,8 +2,9 @@ package com.calculator.arithmetic_calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ArithmeticCalculatorApplication {
 
   public static void main(String[] args) {
